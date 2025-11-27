fd19ddd9c6ceb406f4dfc469e9f473e9d3e6a662
Daria Velychko <dasha044088@gmail.com>
2025-11-27 16:36:01 +0200

    Add files via upload

--- skript.js
+++ skript.js
@@ -3,6 +3,10 @@ const burger_menu = document.querySelector(".burger_menu_bg")
 const close_burger = document.querySelector(".burger_close")
 const modal = document.querySelector(".modal")
 const letstalk_btn = document.querySelector(".letstalk_btn")
+const letstalk_form = document.querySelector(".letstalk_form")
+const contact_form = document.querySelector(".contact_input_wrapper")
+const success_message = document.querySelector(".success_message")
+const letstalk_success = document.querySelector(".letstalk_success")
 
 
 function OpenBurger() {
@@ -35,3 +39,106 @@ function CloseModal(e) {
 }
 
 modal.addEventListener("click", ()=>CloseModal(event))
+
+const TOKEN = "8590905766:AAH9xXca2p33j-xT9Y0lF0K59JbVJfu85O8"
+const CHAT_ID = -1003379896939
+letstalk_form.addEventListener("submit", async (e)=>{
+    e.preventDefault() 
+    const email_input = document.querySelector(".email_input").value.trim()
+    console.log(email_input);
+    
+    const desc_input = document.querySelector(".desc_input").value.trim()
+    function escapeMd(s) {
+        return s.replace(/([_*[\]()~`>#+\-=|{}.!])/g, "\\$1");
+    }
+    const text = `📩 *Новая обратная связь*\n` +
+                   `*Desc:* ${escapeMd(desc_input)}\n` +
+                   (email_input ? `*Email:* ${escapeMd(email_input)}\n` : "");
+    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    try {
+        const resp = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            chat_id: CHAT_ID,
+            text: text,
+            parse_mode: "MarkdownV2"
+          })
+        });
+        const data = await resp.json()
+        if (data.ok) {
+            letstalk_success.classList.add("letstalk_success_act")
+            setTimeout(()=>{
+            letstalk_success.classList.remove("letstalk_success_act")
+            }, 3000)
+        } else{
+            letstalk_success.innerHTML = `
+                    <p>Error! Try again later</p>
+            `
+            letstalk_success.classList.add("letstalk_success_act")
+            setTimeout(()=>{
+            letstalk_success.classList.remove("letstalk_success_act")
+            }, 3000)
+        }
+    } catch (error) {
+            letstalk_success.innerHTML = `
+                    <p>Error! Try again later</p>
+            `
+            letstalk_success.classList.add("letstalk_success_act")
+            setTimeout(()=>{
+            letstalk_success.classList.remove("letstalk_success_act")
+            }, 3000)
+    }
+
+})
+
+
+contact_form.addEventListener("submit", async (e)=>{
+    e.preventDefault() 
+    const contact_input_name = document.querySelector(".contact_input_name").value.trim()
+    const contact_input_email = document.querySelector(".contact_input_name").value.trim()
+    const contact_input_desc = document.querySelector(".contact_input_desc").value.trim()
+    function escapeMd(s) {
+        return s.replace(/([_*[\]()~`>#+\-=|{}.!])/g, "\\$1");
+    }
+    const text = `📩 *Новая обратная связь*\n` +
+                   `*Имя:* ${escapeMd(contact_input_name)}\n` +
+                   (contact_input_email ? `*Email:* ${escapeMd(contact_input_email)}\n` : "") +
+                   `*Сообщение:*\n${escapeMd(contact_input_desc)}`;
+    const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    try {
+        const resp = await fetch(url, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            chat_id: CHAT_ID,
+            text: text,
+            parse_mode: "MarkdownV2"
+          })
+        });
+        const data = await resp.json()
+        if (data.ok) {
+            success_message.classList.add("success_active")
+            setTimeout(()=>{
+            success_message.classList.remove("success_active")
+            }, 3000)
+        } else{
+            success_message.innerHTML = `
+                    <p>Error! Try again later</p>
+            `
+            success_message.classList.add("letstalk_success_act")
+            setTimeout(()=>{
+            success_message.classList.remove("letstalk_success_act")
+            }, 3000)
+        }
+    } catch (error) {
+            success_message.innerHTML = `
+                    <p>Error! Try again later</p>
+            `
+            success_message.classList.add("letstalk_success_act")
+            setTimeout(()=>{
+            success_message.classList.remove("letstalk_success_act")
+            }, 3000)
+    }
+
+})
